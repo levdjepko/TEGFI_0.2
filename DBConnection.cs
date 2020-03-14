b0d0aa2de093ddb5dff9ec84b3e1428ba636db39
Lev <levdjepko@gmail.com>
2020-03-14 11:46:14 -0700

    Initial upload
    Connects to a database

---
+++ DBConnection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace TEGFI_3
+{
+    public static class DBConnection
+    {
+        public static void CheckConnection(string id, string password)
+        {
+            string connetionString = null;
+            MySqlConnection cnn;
+            connetionString = $"Server=3.227.166.251; database=U053QS; UID={id}; password={password}";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+
+        public static bool ConnectToDatabase(string id, string password)
+        {
+            string connetionString = null;
+            MySqlConnection cnn;
+            connetionString = $"Server=3.227.166.251; database=U053QS; UID={id}; password={password}";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Successful login!");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ MainScreen.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TEGFI_3
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exit_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exit_Button
+            // 
+            this.exit_Button.Location = new System.Drawing.Point(659, 402);
+            this.exit_Button.Name = "exit_Button";
+            this.exit_Button.Size = new System.Drawing.Size(75, 23);
+            this.exit_Button.TabIndex = 0;
+            this.exit_Button.Text = "Exit";
+            this.exit_Button.UseVisualStyleBackColor = true;
+            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exit_Button);
+            this.Name = "MainScreen";
+            this.Text = "MainScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exit_Button;
+    }
+}
